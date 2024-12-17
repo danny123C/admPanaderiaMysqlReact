945d3e64df6acf6cd3a7a54a31a21db1bf8babf6
Danny <dannycoro1234@gmail.com>
2024-12-17 13:22:00 -0500

    cambios en las rutasx3

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@ export const DB_USER = process.env.DB_USER || `root`;
 export const DB_PASSWORD = process.env.DB_PASSWORD || `danny123`;
 export const DB_NAME = process.env.DB_NAME || `PanaderiaDB`;
 export const DB_PORT = process.env.DB_PORT || 3306;
-export const URLL_FRONT = process.env.DB_PORT || "http://localhost:5173";
+export const URLL_FRONT = process.env.URLL_FRONT || "http://localhost:5173"; // Corregido aquí
